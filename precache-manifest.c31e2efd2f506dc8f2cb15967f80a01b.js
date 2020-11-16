12341d9dff7a7c1b02263e2771cdb30616b8568c
kevin Gutierrez <kevingutierrez6595gmail.com>
2020-11-16 06:09:39 -0600

    first commit

---
+++ precache-manifest.c31e2efd2f506dc8f2cb15967f80a01b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2505908fe236d57f6bab70f909ceb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1894c3e0b3746a05e5b2",
+    "url": "/static/css/main.01a337c2.chunk.css"
+  },
+  {
+    "revision": "76416454bb14ef33de3f",
+    "url": "/static/js/2.6b61626d.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.6b61626d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1894c3e0b3746a05e5b2",
+    "url": "/static/js/main.cfb3554f.chunk.js"
+  },
+  {
+    "revision": "eb245f5d301250364d57",
+    "url": "/static/js/runtime-main.dffa7f7c.js"
+  },
+  {
+    "revision": "5bb19dd96dc044713177600200df898b",
+    "url": "/static/media/michi.5bb19dd9.jpg"
+  }
+]);
